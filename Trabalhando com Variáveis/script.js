98b727b6519975f7802d0e18e385e52a1cb24641
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-01 14:50:11 -0300

    A diferença de constante e variáveis

--- Trabalhando com Variáveis/script.js
+++ Trabalhando com Variáveis/script.js
@@ -1,3 +1,4 @@
+/*
 //saída
 console.log("Digite o nome do seu jogador: ");
 
@@ -7,3 +8,11 @@ let nickname = "Natasha";
 //concatenando uma mensagem fixa + uma variável
 console.log("Bem-vindo(a) ao jogo, " + nickname + "!");
 console.log(nickname + " entrou no servidor.");
+
+*/
+
+const notificacao = "Pokemon Go diz: ";
+
+//saída
+console.log(notificacao + "tem um novo pokemon na região!")
+console.log(notificacao + "você foi derrotado por um líder de ginásio!")
