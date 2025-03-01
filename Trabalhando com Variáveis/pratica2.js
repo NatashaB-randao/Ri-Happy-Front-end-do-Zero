f1dea69d6f5c002024853fadde93d24ea4985eb2
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-01 15:24:34 -0300

    Exercício de fixação

---
+++ Trabalhando com Variáveis/pratica2.js
@@ -0,0 +1,8 @@
+// nome, idade, numero de telefone, cpf, endereço, recebe pensão?
+
+let nomePessoa = "Maria Clara"
+let idade = 84
+let numeroTelefone = "(11)999999999"
+let cpf = "123.456.789-10"
+let endereco = "Rua das Flores, 123, bairro da jaguatiriça, São Paulo, SP"
+let recebePensao = true
