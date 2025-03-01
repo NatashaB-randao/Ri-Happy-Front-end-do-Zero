7982746b74cacb5c4e938c1c967d97d99c044580
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-01 14:06:03 -0300

    Estrutura do nosso programa

--- Trabalhando com Variáveis/pratica.js
+++
@@ -1,5 +0,0 @@
-console.log("Digite o nome do seu jogador: ");
-
-let nickname = "Natasha";
-
-console.log("Bem-vindo(a) ao jogo, " + nickname + "!");
---
+++ Trabalhando com Variáveis/script.js
@@ -0,0 +1,9 @@
+//saída
+console.log("Digite o nome do seu jogador: ");
+
+//declaração de variável
+let nickname = "Natasha";
+
+//concatenando uma mensagem fixa + uma variável
+console.log("Bem-vindo(a) ao jogo, " + nickname + "!");
+console.log(nickname + " entrou no servidor.");
