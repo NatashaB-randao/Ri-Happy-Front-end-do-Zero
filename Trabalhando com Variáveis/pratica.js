c230c1d0bfc4d7030c88d910423d4b753d95a30f
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-01 15:02:58 -0300

    Colocando seu conhecimento em prática

---
+++ Trabalhando com Variáveis/pratica.js
@@ -0,0 +1,16 @@
+let poteCafe = "Café Pilão";
+let poteAcucar = "Açúcar Cristal";
+let poteBiscoito = "Biscoito Maizena";
+const mensagemDaVovo = "Na cozinha da vovó tem: " 
+
+console.log(mensagemDaVovo + 
+    poteCafe + ", " + 
+    poteAcucar + " e " + 
+    poteBiscoito + ".");
+
+poteCafe = "Café Melitta";
+console.log(mensagemDaVovo + 
+    poteCafe + ", " + 
+    poteAcucar + " e " + 
+    poteBiscoito + ".");
+
