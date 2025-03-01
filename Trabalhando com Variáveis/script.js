085b9968d859ad583013f701c55c27f80bafcd59
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-01 15:14:03 -0300

    Entendendo Strings, Numbers e Booleans

--- Trabalhando com Variáveis/script.js
+++ Trabalhando com Variáveis/script.js
@@ -11,8 +11,18 @@ console.log(nickname + " entrou no servidor.");
 
 */
 
+/*
 const notificacao = "Pokemon Go diz: ";
 
 //saída
 console.log(notificacao + "tem um novo pokemon na região!")
 console.log(notificacao + "você foi derrotado por um líder de ginásio!")
+
+*/
+
+//pokemon
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVida = 45
+let pokemonSexo = "M"
+let selecionavel = true
