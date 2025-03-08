e5b605f84b9d21ddb133bf7a35dd94dbb2211bf7
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-08 15:50:19 -0300

    Trabalhando com Matrizes

--- Aprendendo Lógica de Programação/Criando e Manipulando Vetores e Matrizes/script.js
+++ Aprendendo Lógica de Programação/Criando e Manipulando Vetores e Matrizes/script.js
@@ -11,9 +11,27 @@ console.log(pokemon);
 */
 
 
+/*
 let pokemon = ["Pikachu", "Charmander", "Bulbassauro", "Squirtle", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Magikarp", "Eevee"];
 
 pokemon.shift();
 pokemon.pop();
 
 console.log(pokemon);
+*/
+
+let pokemon = ["Pikachu", "Charmander", "Bulbassauro", "Squirtle", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Magikarp", "Eevee"];
+let timePokemon = [
+    ["Pikachu", "M", 1],
+    ["Charmander", "F", 4],
+    ["Bulbassauro", "M", 2],
+    ["Squirtle", "F", 7],
+    ["Jigglypuff", "M", 5],
+    ["Meowth", "F", 3],
+    ["Psyduck", "M", 2],
+    ["Snorlax", "F", 4],
+    ["Magikarp", "M", 3],
+    ["Eevee", "F", 1]
+]
+
+console.log(" O Pokemón " + timePokemon[1][0] + " é do sexo " + timePokemon[1][1] + " e está na posição " + timePokemon[2][2]);
