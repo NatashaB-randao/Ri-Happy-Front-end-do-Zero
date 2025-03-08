cf8f090cffa143677f8382a49b91eba227000d7f
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-08 15:25:14 -0300

    Vantagens de se utilizar vetores

--- Aprendendo Lógica de Programação/Criando e Manipulando Vetores e Matrizes/script.js
+++ Aprendendo Lógica de Programação/Criando e Manipulando Vetores e Matrizes/script.js
@@ -1,7 +1,19 @@
 // Guardando coisas em armários ao invés de caixas
 
+/*
 let pokemon = ["Pikachu", "Charmander", "Bulbassauro", "Squirtle", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Magikarp", "Eevee"];
 console.log(pokemon);
 console.log(pokemon[0]);
 console.log(pokemon[1]);
 console.log(pokemon[2]);
+
+console.log(pokemon);
+*/
+
+
+let pokemon = ["Pikachu", "Charmander", "Bulbassauro", "Squirtle", "Jigglypuff", "Meowth", "Psyduck", "Snorlax", "Magikarp", "Eevee"];
+
+pokemon.shift();
+pokemon.pop();
+
+console.log(pokemon);
