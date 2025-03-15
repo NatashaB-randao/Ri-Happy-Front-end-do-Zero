c0515526c8d94a86b0492d71e4d82c9130131b82
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-15 14:46:03 -0300

    IF

--- Aprendendo Lógica de Programação/3 - Trabalhando Com Estruturas Condicionais/script.js
+++ Aprendendo Lógica de Programação/3 - Trabalhando Com Estruturas Condicionais/script.js
@@ -1,7 +1,18 @@
+/*
 let ehLigado = false;   
 
 if (ehLigado) {
     console.log("O carro está ligado!");
 } else {
     console.log("O carro está desligado!");
+} 
+*/
+
+let possuiOvos = false;
+let itensComprados = "";
+
+if (possuiOvos) {
+    itensComprados = "leite"
 }
+
+console.log("Item Comprado: " + itensComprados);
