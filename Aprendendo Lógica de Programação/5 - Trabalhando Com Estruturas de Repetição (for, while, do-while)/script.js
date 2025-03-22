0631b9b627eaa45388c7be57488097af2c85edd4
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-22 16:29:39 -0300

    Trabalhando Com Estruturas de Repetição (for, while, do-while)

---
+++ Aprendendo Lógica de Programação/5 - Trabalhando Com Estruturas de Repetição (for, while, do-while)/script.js
@@ -0,0 +1,39 @@
+// For
+
+/*
+for (let cont = 0; contador < 4; contador++) {
+    console.log(contador);
+    console.log('Aumentando o contador');
+}
+*/
+
+/*
+let pontosDeVida = 0;
+
+for (let i = 0; i < 11; i++) {
+    pontosDeVida += 1;
+    console.log("Tomou poção mágica " + i);
+}
+
+console.log(pontosDeVida + "Totais");
+*/
+
+
+/*
+// While
+let contador = 0;
+
+while (contador < 3) {
+    console.log("Olá");
+    contador++;
+}
+*/
+
+
+// Do While
+let contador = 0;
+
+do {
+    console.log("Olá");
+    contador++;
+} while (contador < 3);
