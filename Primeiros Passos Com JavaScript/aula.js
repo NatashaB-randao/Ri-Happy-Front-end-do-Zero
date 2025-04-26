7d9e2c7f955e0fee8d09832ef1b955643f45c79a
NatashaB-randao <natashabrandao57@hotmail.com>
2025-04-26 14:34:05 -0300

    Estrutura Condicional Com If e Else

--- Primeiros Passos Com JavaScript/aula.js
+++ Primeiros Passos Com JavaScript/aula.js
@@ -1,3 +1,6 @@
+// Variáveis e Operadores
+
+/*
 let variavel = 10;
 
 console.log(variavel);
@@ -6,4 +9,28 @@ console.log(variavel);
 variavel = 20;
 
 console.log(variavel);
+
+*/
+
+
+// Estruturas Condicionais
+
+/*
+const numero = 10;
+const numeroPar = (numero % 2) === 0;
+
+console.log(numeroPar);
+
+*/
+
+const numero = 3;
+const numeroPar = (numero % 2) === 0;
+console.log(numeroPar);
+
+if (numeroPar) {
+    console.log("O número é par.");
+
+}else {
+    console.log("O número é ímpar.");
+}
 
